6f219723132bdce9b177945fe23146eb85c8438a
Nikolay Borovik <nik.Borovik@bk.ru>
2023-04-17 17:36:40 +1000

    task16 done

--- 16.py
+++ 16.py
@@ -3,4 +3,17 @@
 # натуральное число N – количество элементов в массиве. В последующих
 # строках записаны N целых чисел Ai
 # . Последняя строка содержит число X
+from random import randint
+N = int(input("Enter size of the list: "))
+X = int(input("Enter number to seek: "))
 
+list = []
+for i in range(N):
+    list.append(randint(0,9))
+print(list)
+
+count = 0
+for i in list:
+    if i == X:
+        count +=1
+print(f"This number occurs {count} times in the list")
